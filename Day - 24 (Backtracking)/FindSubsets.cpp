bf382c6ab9903c1ceda313cb8ec1ffcd80a63260
sai-dushyant <126763864+SaiDushyant@users.noreply.github.com>
2025-06-24 16:01:24 +0530

    Finding Substrings

--- Day - 24 (Backtracking)/FindSubsets.cpp
+++ Day - 24 (Backtracking)/FindSubsets.cpp
@@ -3,11 +3,26 @@
 #include <vector>
 using namespace std;
 
-void findSubset()
+void findSubset(string str, string subset)
 {
+    if (str.size() == 0)
+    {
+        cout << subset << "\n";
+        return;
+    }
+    char ch = str[0];
+
+    // Yes Choice
+    findSubset(str.substr(1, str.size() - 1), subset + ch);
+
+    // No Choice
+    findSubset(str.substr(1, str.size() - 1), subset);
 }
 
 int main()
 {
+    string str = "abc";
+    string subset = "";
+    findSubset(str, subset);
     return 0;
 }
